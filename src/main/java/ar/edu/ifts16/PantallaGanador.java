b27ef3554ae3b81a7eacfe4e8b1df0cc81a6132c
NoraBarria <norabarria1@gmail.com>
2020-12-08 01:02:33 -0300

    Agrego clase PantallaGanador e imagen ganaste

---
+++ src/main/java/ar/edu/ifts16/PantallaGanador.java
@@ -0,0 +1,28 @@
+package ar.edu.ifts16;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+
+public class PantallaGanador extends PantallaImagen {
+	private Puntaje puntaje;
+
+    public PantallaGanador(int ancho, int largo, String resource, Puntaje puntaje) {
+        super(ancho, largo, resource);
+        this.puntaje = puntaje;
+    }
+
+    public void dibujarse(Graphics graphics) {
+        super.dibujarse(graphics);
+        String mensajePuntos = " puntos";
+        if (puntaje.getPuntajeActual() == 5) {//362
+            mensajePuntos += "s";
+        mostrarMensaje(graphics, "Puntaje perfecto! " + puntaje.getPuntajeActual() + mensajePuntos);
+    }}
+
+    private void mostrarMensaje(Graphics g, String mensaje) {
+        g.setColor(Color.blue);
+        g.setFont(new Font("Arial", 8, 30));
+        g.drawString(mensaje, 10, 40);
+    }
+}
